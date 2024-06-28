92b81ab755786616cbdda21799742dc7e2c39d3b
ngdpforreal <168348967+ngdpforreal@users.noreply.github.com>
2024-06-28 15:06:25 +0700

    Create Script.sh

---
+++ Script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+echo Make sure you have permission on your termux.
+echo Taking Status from your android folder.
+
+# Check if the script is running on Android
+if [ -e /system/bin/sh ]; then
+    echo "Android platform detected. Continuing script..."
+    cd /storage/emulated/0/media/com.whatsapp/Media/Whatsapp/.Statuses
+mkdir /sdcard/status
+mv * /sdacrd/status
+echo all done! just check your status on /storage/emulated/0/status
+else
+    echo "Sorry! This Script just for Android Device that has whatsaap"
+    exit 1
+fi
+
